030d81f0e117d7af6a899342069831a8160e6d05
clerdee <clerdeecruz@gmail.com>
2025-06-19 02:42:14 +0800

    connect pls pls pls

--- app.js
+++ app.js
@@ -6,14 +6,18 @@ const path = require('path');
 const items = require('./routes/item')
 // const users = require('./routes/user')
 
-app.use(cors())
-
-app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/images', express.static(path.join(__dirname, 'images')))
 
 app.use('/api/v1', items);
 // app.use('/api/v1', users);
+
+// Server
+// app.listen(3000, '0.0.0.0', () => {
+//   console.log('Backend running on http://192.168.1.4:3000');
+// });
 
 module.exports = app
--- index.js
+++ index.js
@@ -8,3 +8,10 @@ app.listen(PORT, () => {
     console.log(PORT)
   console.log(`Server is running on port ${PORT}`);
 });
+
+// const PORT = 3000;
+// const HOST = '0.0.0.0';
+
+// app.listen(PORT, HOST, () => {
+//   console.log(`Backend running on http://${HOST}:${PORT}`);
+// });

824cd66151093f73c1921aaa90673fe768988939
clerdee <clerdeecruz@gmail.com>
2025-07-07 16:29:09 +0800

    automatic port running-backend&frontend

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const path = require('path');
+const open = require('open');
+const port = 4000;
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+  open(`http://localhost:${port}/home.html`);
+});
 
 const items = require('./routes/item')
 // const users = require('./routes/user')
@@ -10,6 +17,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname, '../frontend-toyblox')));
 app.use('/images', express.static(path.join(__dirname, 'images')))
 
 app.use('/api/v1', items);
